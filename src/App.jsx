d1374ff1ff79d5852607320dbfeb80a9f2da6b49
abhicodes13 <68747686+abhicodes13@users.noreply.github.com>
2023-08-01 00:47:36 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import './App.css';
+import MyApp from './MyApp';
+
+export default function App() {
+  return (
+<MyApp/>
+  )
+}
---
+++ src/MyApp.jsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+
+export default function MyApp() {
+  return (
+    <Button variant="text">Text</Button>
+  );
+}
