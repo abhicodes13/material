5e0a9366bf202ff3fe0af7f992a5745e36884fa6
abhicodes13 <68747686+abhicodes13@users.noreply.github.com>
2023-08-01 01:02:31 +0000

    Added lab library and coded a timeline

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import './App.css';
 import ImageLi from './ImageLi.jsx';
+import HelloTime from './HelloTime.jsx';
 
 export default function App() {
   return (
+    <div>
+      
 <ImageLi/>
+      <HelloTime/>
+      </div>
   )
 }
